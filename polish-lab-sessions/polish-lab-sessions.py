f1342f3b8aa1c67afdcf3f52619a429813ce638a
Agata Kozioł <agata.koziol.98@gmail.com>
2024-04-05 16:56:35 +0200

    Update polish-lab-sessions.py

--- polish-lab-sessions/polish-lab-sessions.py
+++ polish-lab-sessions/polish-lab-sessions.py
@@ -2,7 +2,7 @@ import whisper
 import ssl
 ssl._create_default_https_context = ssl._create_unverified_context
 
-model = whisper.load_model("medium")
+model = whisper.load_model("small")
 #result = model.transcribe("../polish-data/lab-sessions/77003_1_manipulative_kam1.wav")
 result = model.transcribe("../polish-data/home-recordings/V20211121-091352.mp3")
 print(result["text"])
